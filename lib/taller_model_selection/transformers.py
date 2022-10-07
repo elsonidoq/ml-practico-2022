75eb6753f59f104fb68413b0496a40213d1230eb
Pablo Rodriguez Zivic <pablo.rzivic@mercadolibre.com>
2022-10-07 20:28:30 -0300

    target encoder

--- lib/taller_model_selection/transformers.py
+++ lib/taller_model_selection/transformers.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 
@@ -7,6 +9,7 @@ class FeatureProjection(BaseEstimator, TransformerMixin):
     Recibe una lista de campos a proyectar, y los proyecta como listas o como diccionarios
     Ver notebook 02
     """
+
     def __init__(self, fields, as_dict=False, convert_na=True):
         self.fields = fields
         self.as_dict = as_dict
@@ -26,11 +29,51 @@ class FeatureProjection(BaseEstimator, TransformerMixin):
         return res
 
 
+class TargetEncoder(BaseEstimator, TransformerMixin):
+    """
+    Encodea una categorica como un vector de cuatro dimensiones
+    [mean(y), std(y), percentile(y, 5), percentile(y, 95)]
+    """
+
+    def __init__(self, categorical_field):
+        self.categorical_field = categorical_field
+        self.stats_ = None
+        self.default_stats = None
+
+    def fit(self, X, y):
+        values = defaultdict(list)
+        for i, x in enumerate(X):
+            values[x[self.categorical_field]].append(y[i])
+
+        self.stats_ = {}
+        for cat_value, tar_values in values.items():
+            tar_values = np.asarray(tar_values)
+            self.stats_[cat_value] = [
+                np.mean(tar_values), np.std(tar_values),
+                np.percentile(tar_values, 90), np.percentile(tar_values, 10)
+            ]
+
+        self.default_stats_ = [
+            np.mean(y), np.std(y),
+            np.percentile(y, 90), np.percentile(y, 10)
+        ]
+        # Siempre hay que devolver self
+        return self
+
+    def transform(self, X):
+        res = []
+        for i, doc in enumerate(X):
+            vector = self.stats_.get(doc[self.categorical_field], self.default_stats_)
+            res.append(vector)
+        return res
+
+
 class PretrainedFastTextTransformer(BaseEstimator, TransformerMixin):
     """
     Dado un nombre de archivo de un modelo de fasttext (ver notebook 4a y 4b) y un campo de texto
     Genera features del campo textual a traves del modelo de fasttext
     """
+
     def __init__(self, fname, field):
         self.fname = fname
         self.field = field
@@ -64,4 +107,3 @@ class PretrainedFastTextTransformer(BaseEstimator, TransformerMixin):
 
     def __setstate__(self, state):
         vars(self).update(state)
-
